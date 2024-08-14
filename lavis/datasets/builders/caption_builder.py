2eca461e55843323a1d5a5b77b058af509e41fad
Khoa D. Vo <khoa.synthesis@gmail.com>
2024-08-14 22:35:10 +0700

    v1

---
+++ lavis/datasets/builders/caption_builder.py
@@ -0,0 +1,23 @@
+"""
+ Copyright (c) 2022, salesforce.com, inc.
+ All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+ For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
+from lavis.datasets.builders.base_dataset_builder import BaseDatasetBuilder
+from lavis.datasets.datasets.coco_caption_datasets import (
+    COCOCapDataset,
+    COCOCapEvalDataset,
+)
+
+from lavis.common.registry import registry
+
+@registry.register_builder("coco_caption")
+class COCOCapBuilder(BaseDatasetBuilder):
+    train_dataset_cls = COCOCapDataset
+    eval_dataset_cls = COCOCapEvalDataset
+
+    DATASET_CONFIG_DICT = {
+        "default": "configs/datasets/coco/defaults_cap.yaml",
+    }
